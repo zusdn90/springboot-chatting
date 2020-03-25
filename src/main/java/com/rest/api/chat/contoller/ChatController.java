1daafe17964c52045cf8b78cd50b131c83f596bd
hwhong <hwhong@bimatrix.co.kr>
2020-03-25 17:26:58 +0900

    websocket을 활용한 간단한 chatting server 구현

---
+++ src/main/java/com/rest/api/chat/contoller/ChatController.java
@@ -0,0 +1,25 @@
+package com.rest.api.chat.contoller;
+
+import com.rest.api.chat.dto.ChatRoom;
+import com.rest.api.chat.service.ChatService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/chat")
+public class ChatController {
+    private final ChatService chatService;
+
+    @PostMapping
+    public ChatRoom createRoom(@RequestParam("room") String name) {
+        return chatService.createRoom(name);
+    }
+
+    @GetMapping
+    public List<ChatRoom> findAllRoom() {
+        return chatService.findAllRoom();
+    }
+}
---
+++ src/main/java/com/rest/api/chat/handler/WebSocketChatHandler.java
@@ -0,0 +1,33 @@
+package com.rest.api.chat.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rest.api.chat.dto.ChatMessage;
+import com.rest.api.chat.dto.ChatRoom;
+import com.rest.api.chat.service.ChatService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class WebSocketChatHandler extends TextWebSocketHandler {
+
+    private final ObjectMapper objectMapper;
+    private final ChatService chatService;
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        String payload = message.getPayload();
+        log.info("payload {}", payload);
+        //TextMessage textMessage = new TextMessage("Welcome chatting server~~~");
+        //session.sendMessage(textMessage);
+
+        ChatMessage chatMessage = objectMapper.readValue(payload, ChatMessage.class);
+        ChatRoom room = chatService.findRoomById(chatMessage.getRoomId());
+        room.handleActions(session, chatMessage, chatService);
+    }
+}
